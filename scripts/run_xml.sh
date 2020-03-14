e852a5baa092a7d5a49748ae71cd41a3dbc6c0da
Sangwoo Han <realappsdev@gmail.com>
2020-03-14 23:24:51 +0900

    Update run_xml.sh

--- scripts/run_xml.sh
+++ scripts/run_xml.sh
@@ -7,4 +7,7 @@ elif [ $3 ]; then
   SUFFIX2="-s -$3"
 fi
 
-python ensemble.py -p results/$2-$1 -t 5 ${SUFFIX2:-$SUFFIX} $INPUT_SUFFIX
+python main.py --data-cnf configure/datasets/$1.yaml --model-cnf configure/models/$2-$1.yaml -t 0 $SUFFIX && \
+python main.py --data-cnf configure/datasets/$1.yaml --model-cnf configure/models/$2-$1.yaml -t 1 $SUFFIX && \
+python main.py --data-cnf configure/datasets/$1.yaml --model-cnf configure/models/$2-$1.yaml -t 2 $SUFFIX && \
+python ensemble.py -p results/$2-$1 -t 3 ${SUFFIX2:-$SUFFIX} $INPUT_SUFFIX
